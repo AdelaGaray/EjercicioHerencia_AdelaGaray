f67a5f01184c973048198aac8a233af082963499
gadel <gadel@ADELA>
2024-07-25 22:04:13 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioHerencia_AdelaGaray/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Crear una instancia de la clase OperacionesImpresion
+        double radio = 5;
+        double altura = 10;
+        OperacionesImpresion operaciones = new OperacionesImpresion(radio, altura);
+
+        // Imprimir los resultados
+        operaciones.Imprimir();
+
+        // Mantener la consola abierta
+        Console.ReadLine();
+    }
+}
+
---
+++ EjercicioHerencia_AdelaGaray/operacionimpresiones.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class OperacionesImpresion : Operaciones
+{
+    private double _radio;
+    private double _altura;
+
+    // Constructor ///
+    public OperacionesImpresion(double radio, double altura = 0)
+    {
+        _radio = radio;
+        _altura = altura;
+    }
+
+    // Método para imprimir resultados /// 
+    public void Imprimir()
+    {
+        Console.WriteLine($"Área del círculo con radio {_radio}: {CalcularAreaCirculo(_radio)}");
+        Console.WriteLine($"Volumen del cilindro con radio {_radio} y altura {_altura}: {CalcularVolumenCilindro(_radio, _altura)}");
+        Console.WriteLine($"Longitud de la circunferencia con radio {_radio}: {CalcularLongitudCircunferencia(_radio)}");
+    }
+}
